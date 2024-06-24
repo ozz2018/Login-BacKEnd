f57dbfc2d8f84b6cc4087f7931dda7be5ea550fb
Oskar Solano <157350576+ozz2018@users.noreply.github.com>
2024-06-24 13:47:43 -0600

    test

--- login/src/App.jsx
+++ login/src/App.jsx
@@ -1,82 +1,105 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import clsx from 'clsx'
-import { useForm } from 'react-hook-form'
-import { dataValidation } from './api'
-import { Toaster, toast } from 'sonner'
+import { useForm } from "react-hook-form";
+import { login } from "./api";
+import { Toaster, toast } from "sonner";
 
-function App() {
- 
-  const { register, handleSubmit, formState: { erros, isValid, isSubmitted}, reset, setFocus } = useForm();
 
-  function onSumbit(data) {
+export default function App() {
     
-      dataValidation(data)
-        .then((response) => {
-          return response.json()
-        })
-        .then((json) => {
-          if(json?.data?.token){
-            localStorage.setItem('token', json.data.token)
-            setFocus('email');
-            toast.success('Logged')
-            reset();
-          }else{
-            toast.warning('Log Failed')
-          }
-            
-        })
-        .catch((error) =>{
-          console.error('Error', error);
-          toast.error('Error ' + error.message)
-          
-        })
-    } 
-  return (
-    <>
-      <main className='w-full min-h-screen flex flex-col gap-4'>
-        <Toaster position='top-right' richColors />
-        <p className='w-full bg-slate-600 text-white font-bold text-center p-2'> Login Form </p>
+  const { register, handleSubmit } = useForm();
+  
+  async function submit(data) {
+    console.log('datos a usar', data.email, data.password)
+    try {
+      const token = await login({ email: data.email, password: data.password });
+      localStorage.setItem("token", token);
+      toast.success("Login successful");
+    } catch (error) {
+      toast.error("Login failed:", error.message);
+    }
+  }
 
-        <form 
-          className='flex flex-col gap-4 items-center'
-          onSubmit={handleSubmit(onSumbit)}
-        >
-          <p className=''> Email </p>
-          <input 
-            className='bg-white  text-black w-full max-w-screen-sm p-2'
-            type='text'
-            placeholder='Ingresa tu correo electronico'
-            required
-            {...register('email', {
-              required: { value: true, message: "Campo requerido" },
-              minLength: { value: 3, message: "Minimo 3 caracteres" },
-              maxLength: { value: 50, message: "Máximo  caracteres" },
-              pattern: { value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,message: "Correo Invalido"}
-            })}
-          > </input>
-          <p> Password </p>
-          <input 
-            className='bg-white text-black w-full max-w-screen-sm p-2'
-            type='text'
-            placeholder='Ingres password'
-            required
-            {...register('password', {
-              required: { value: true, message: "Campo requerido" },
-              minLength: { value: 3, message: "Minimo 3 caracteres" },
-              maxLength: { value: 50, message: "Máximo  caracteres" },
-            })}
-          >
-          </input>
-          <button
-            className="text-black px-3 rounded bg-white disabled:bg-stone-400" 
-          >
-            + Agregar
-          </button> 
+  return (
+    <div className="flex min-h-[100dvh] items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
+      <Toaster position="top-right" />
+      <div className="w-full max-w-md space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-foreground">
+            Inicia sesión en tu cuenta
+          </h2>
+          <p className="mt-2 text-center text-sm text-muted-foreground">
+            O{" "}
+            <a
+              className="font-medium text-primary hover:text-primary/80"
+              href="#"
+              rel="ugc"
+            >
+              regístrate
+            </a>
+          </p>
+        </div>
+        <form className="space-y-6" action="#" onSubmit={handleSubmit(submit)}>
+          <div>
+            <label className="peer-disabled:cursor-not-allowed peer-disabled:opacity-70 block text-sm font-medium text-muted-foreground">
+              Correo electrónico
+            </label>
+            <div className="mt-1">
+              <input
+                className="h-10 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 block w-full appearance-none rounded-md border border-input bg-background px-3 py-2 placeholder-muted-foreground shadow-sm focus:border-primary focus:outline-none focus:ring-primary sm:text-sm"
+                {...register("email", {
+                  required: { value: true, message: "Campo requerido" },
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                    message: "Correo inválido",
+                  },
+                })}
+              />
+            </div>
+          </div>
+          <div>
+            <label className="peer-disabled:cursor-not-allowed peer-disabled:opacity-70 block text-sm font-medium text-muted-foreground">
+              Contraseña
+            </label>
+            <div className="mt-1">
+              <input
+                className="h-10 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 block w-full appearance-none rounded-md border border-input bg-background px-3 py-2 placeholder-muted-foreground shadow-sm focus:border-primary focus:outline-none focus:ring-primary sm:text-sm"
+                {...register("password", {
+                  required: { value: true, message: "Campo requerido" },
+                })}
+              />
+            </div>
+          </div>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <input
+                className="flex border bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 h-4 w-4 rounded border-gray-300 text-primary focus:ring-primary"
+                id="remember-me"
+                type="checkbox"
+                name="remember-me"
+              />
+              <label className="font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70 ml-2 block text-sm text-muted-foreground">
+                Recuérdame
+              </label>
+            </div>
+            <div className="text-sm">
+              <a
+                className="font-medium text-primary hover:text-primary/80"
+                href="#"
+                rel="ugc"
+              >
+                ¿Olvidaste tu contraseña?
+              </a>
+            </div>
+          </div>
+          <div>
+            <button
+              className="items-center whitespace-nowrap ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 flex w-full justify-center rounded-md bg-primary py-2 px-4 text-sm font-medium text-primary-foreground shadow-sm hover:bg-primary/80 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
+              type="submit"
+            >
+              Iniciar sesión
+            </button>
+          </div>
         </form>
-        
-      </main>
-    </>
-  )
+      </div>
+    </div>
+  );
 }
-export default App
--- login/src/api.js
+++ login/src/api.js
@@ -1,12 +1,18 @@
 const API_URL = "https://desafiobackend33-1.onrender.com"
+//const LOGIN_URL = "https://desafio-backend-olsr.onrender.com";
+   
 
-
-export function dataValidation(data){
-
-    return fetch(`${API_URL}/auth/login`,{
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify(data)
-    })
-    
-}
+export const login = async ({ email, password }) => {
+    const response = await fetch(`${LOGIN_URL}/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    if (!response.ok) {
+      throw new Error("Login failed");
+    }
+    const data = await response.json();
+    return data.data.token; // Suponiendo que el token está en data.token
+  }
